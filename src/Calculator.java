4b3d5379802d91795c8110f3566bb9e23416cf3a
kek228lol666 <hohalinka@gmail.com>
2026-01-15 14:13:58 +0300

    Update Calculator.java
    Добавил метод squareRoot()  (изменение через GitHub)

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,7 @@
 public class Calculator {
 }
 public double power(double a, double b){
-    return Math.pow(a , b)
-}
+    return Math.pow(a , b)}
+        public double squareRoot(double x){
+            return 0;
+}    
